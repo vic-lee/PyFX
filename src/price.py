135d66708bc29bdb120a9b5824c804ca10e8ddff
vic-lee <viclee@nyu.edu>
2019-03-17 23:01:11 -0400

    Create price class

---
+++ src/price.py
@@ -0,0 +1,10 @@
+class Price: 
+    def __init__(self, price, time):
+        self.price = price
+        self.time = time
+
+    def get_time(self):
+        return self.time
+
+    def get_price(self):
+        return self.price
