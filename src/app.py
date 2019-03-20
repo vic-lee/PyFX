040c06875b78cd29c0e9c67bc4da1fa42da1d14b
vic-lee <viclee@nyu.edu>
2019-03-20 18:35:39 +0100

    Create pip movement setup

--- src/app.py
+++ src/app.py
@@ -10,18 +10,22 @@ def main():
     package = setup_fpaths()
     minute_data = package[DataReader.MINUTELY]
 
+    pipmvmts = setup_pip_movement_obj(minute_data)
+
+
+def setup_pip_movement_obj(minute_data):
     pip_movement_config = {
         DurationPipMovmentToPrice.TIME_RANGE: TimeRangeInDay(
             start_time=time(hour=10, minute=30),
             end_time=time(hour=11, minute=2)
         ),
-        DurationPipMovmentToPrice.BENCHMARK_TIMES: "",
+        DurationPipMovmentToPrice.BENCHMARK_TIMES: [
+            time(hour=10, minute=30), time(hour=10, minute=45)
+        ],
     }
 
-    pip_movements = DurationPipMovmentToPrice(
-        minute_price_df=minute_data, 
-        config=pip_movement_config
-    )
+    return DurationPipMovmentToPrice(
+        minute_price_df=minute_data, config=pip_movement_config)
 
 
 def setup_fpaths():
