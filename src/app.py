4f74e0837e3e9fb9e9e82db777241484e2ad6e4c
vic-lee <viclee@nyu.edu>
2019-03-20 18:19:28 +0100

    Initialize app.py

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from os.path import abspath
+
+from datareader import DataReader
+from durationpricemvmt import DurationPipMovmentToPrice
+
+def main():
+
+    in_fpaths = {
+        DataReader.FIX: abspath("../data/datasrc/fix1819.csv"), 
+        DataReader.MINUTELY: abspath("../data/datasrc/GBPUSD_2018.csv"),
+        DataReader.DAILY: abspath("../data/datasrc/gbp_daily.xlsx")  
+    }
+
+    fx_reader = DataReader(in_fpaths)
+    package = fx_reader.read_data()
+    minute_data = package[DataReader.MINUTELY]
+
+    pip_movement_config = {
+        DurationPipMovmentToPrice.TIME_RANGE: "",
+        DurationPipMovmentToPrice.BENCHMARK_TIMES: "",
+    }
+
+    pip_movements = DurationPipMovmentToPrice(
+        minute_price_df=minute_data, 
+        config=pip_movement_config
+    )
+
+
+
+if __name__ == '__main__':
+    main()
