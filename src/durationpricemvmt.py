a213508ccd22854ead43499309fa41cb0f647af9
vic-lee <viclee@nyu.edu>
2019-03-20 18:19:19 +0100

    Add keydef variables for config

--- src/durationpricemvmt.py
+++ src/durationpricemvmt.py
@@ -7,15 +7,16 @@ class DurationPipMovmentToPrice:
     """This class finds the daily price movements within a period of time. 
     It is used in conjunction with DayPipMovmentToPrice class.
     """
+
+    TIME_RANGE = "time range"
+    BENCHMARK_TIMES = "benchmark_times"
+
     def __init__(self, minute_price_df, config):
         self.minute_price_df = minute_price_df    # minutely data
         self.max_price_movements = []   # an array of DayMovements
         self.current_date = None
         self.time_range = config.time_range
         self.benchmark_times = config.benchmark_times
-
-        #config.time_range
-        #config.benchmarks
 
         # time_start = datetime(year=1970, month=1, day=1, hour=10, minute=30)
         # time_end = datetime(year=1970, month=1, day=1, hour=11, minute=2)
@@ -27,21 +28,21 @@ class DurationPipMovmentToPrice:
         Algorithm: 
         For each day, perform max day price movement check. 
         """
+        benchmark_prices = {}
 
         for index, row in self.minute_price_df.iterrows():
             current_price = Price(price=row['val'], time=index)
-            benchmark_prices = {}
             if self._is_row_new_day(date=index):
                 self._update_current_date(date=index)
                 benchmark_prices = self._generate_benchmark_prices()
 
-                day_price_movements = DayPipMovmentToPrice(
+                price_mvmt_day_obj = DayPipMovmentToPrice(
                     date=self.current_date, 
                     benchmark_price=current_price, 
                     time_range=self.time_range
                 )
 
-            day_price_movements.update_max_pip(current_price)
+            price_mvmt_day_obj.update_max_pip(current_price)
 
     
     def _is_row_new_day(self, date):
