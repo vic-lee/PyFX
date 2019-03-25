f57f50efa0ee90b8593003d30fd268fea3aab7df
vic-lee <viclee@nyu.edu>
2019-03-25 21:02:34 +0100

    Create method for excel export

--- src/datawriter.py
+++ src/datawriter.py
@@ -1,3 +1,17 @@
+import pandas as pd
+
 class DataWriter: 
-    def __init__(self):
-        pass
+    
+    def __init__(self, df, default_fname="dataout"):
+        self._df = df
+        self._default_fname = default_fname
+        self._default_fname_xlsx = self._default_fname + ".xlsx"
+        self._default_fname_csv = self._default_fname + ".csv"
+
+
+    def df_to_xlsx(self):
+        with pd.ExcelWriter(self._default_fname_xlsx, engine='xlsxwriter') as writer: 
+            self._df.to_excel(writer, sheet_name="max_pip_mvmts")
+            sheet = writer.sheets['max_pip_mvmts']
+            wide_col = 20
+            sheet.set_column(0, len(self._df.columns), wide_col)
