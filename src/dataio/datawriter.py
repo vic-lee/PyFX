78a62df52416a8ebab5b67f5c31d3b146a753f5f
vic-lee <viclee@nyu.edu>
2019-04-07 12:17:06 -0400

    Add timestamp suffix

--- src/dataio/datawriter.py
+++ src/dataio/datawriter.py
@@ -1,16 +1,25 @@
 import pandas as pd
+from datetime import datetime
 
-class DataWriter: 
-    
-    def __init__(self, df, filename="dataout"):
+
+class DataWriter:
+
+    def __init__(self, df, filename="../data/dataout/dataout"):
         self._df = df
         self._default_fname = filename
-        self._default_fname_xlsx = self._default_fname + ".xlsx"
-        self._default_fname_csv = self._default_fname + ".csv"
 
+        fname_suffix = self._generate_time_suffix()
+
+        self._default_fname_xlsx = self._default_fname + fname_suffix + ".xlsx"
+        self._default_fname_csv = self._default_fname + fname_suffix + ".csv"
+
+    def _generate_time_suffix(self):
+        now = datetime.now()
+        fname_suffix = now.strftime("_%Y%m%d_%H%M%S")
+        return fname_suffix
 
     def df_to_xlsx(self):
-        with pd.ExcelWriter(self._default_fname_xlsx, engine='xlsxwriter') as writer: 
+        with pd.ExcelWriter(self._default_fname_xlsx, engine='xlsxwriter') as writer:
             self._df.to_excel(writer, sheet_name="max_pip_mvmts")
             sheet = writer.sheets['max_pip_mvmts']
             wide_col = 20
