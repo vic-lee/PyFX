fa8d2e416283b499a973f5e4d5b0a84bae9149e5
vic-lee <viclee@nyu.edu>
2019-04-14 23:42:18 -0400

    Chore: reformatting spacing

--- src/metrics/minutely_data.py
+++ src/metrics/minutely_data.py
@@ -10,16 +10,14 @@ class MinutelyData(Metric):
         self.specs = specs
         self.df = self._generate_output_df()
 
-
     def _generate_output_df(self):
         df_list = []
 
         for spec in self.specs:
             df_for_spec = self._generate_period_prices_df(spec)
             df_list.append(df_for_spec)
-        
-        return self._join_minute_dfs(df_list)
 
+        return self._join_minute_dfs(df_list)
 
     def _generate_period_prices_df(self, spec):
         df_list = []
@@ -32,7 +30,8 @@ class MinutelyData(Metric):
         while time_cur <= end_time:
 
             for m in metric_to_include:
-                df = self.full_minute_price_df.between_time(time_cur, time_cur)[m]
+                df = self.full_minute_price_df.between_time(
+                    time_cur, time_cur)[m]
                 df = df.rename("{} {}".format(str(time_cur), m))
                 df.index = df.index.normalize()
                 df_list.append(df)
@@ -40,7 +39,6 @@ class MinutelyData(Metric):
             time_cur = self.incr_one_min(time_cur)
 
         return self._join_minute_dfs(df_list)
-
 
     def _join_minute_dfs(self, df_list):
         df_out = pd.DataFrame()
@@ -51,15 +49,14 @@ class MinutelyData(Metric):
     @staticmethod
     def incr_one_min(time_cur):
         old_min = time_cur.minute
-        if old_min == 59: 
+        if old_min == 59:
             new_min = 0
             new_hour = time_cur.hour + 1
-        else: 
+        else:
             new_min = old_min + 1
             new_hour = time_cur.hour
         time_cur = time(hour=new_hour, minute=new_min, second=time_cur.second)
         return time_cur
 
-    
     def to_df(self):
         return self.df
