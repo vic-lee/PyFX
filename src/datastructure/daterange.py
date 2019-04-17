1190223a0e4c98f15973c9f2c07818e7b3357f8d
vic-lee <viclee@nyu.edu>
2019-04-16 20:27:01 -0400

    Minor formatting change

--- src/datastructure/daterange.py
+++ src/datastructure/daterange.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 class DateRange:
     """This class denotes the range within which the algorithm performs
     its analysis. It has two attributes / reasons to change: 
@@ -7,10 +8,10 @@ class DateRange:
 
     Args: both start_date and end_date are Date objects
     """
+
     def __init__(self, start_date: date, end_date: date):
         self.start_date = start_date
         self.end_date = end_date
-
 
     def is_datetime_in_range(self, date):
         return date >= self.start_date and date <= self.end_date
