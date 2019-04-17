b533218756b1b0306164174ae5808ad9887f9a5b
vic-lee <viclee@nyu.edu>
2019-04-17 18:11:13 -0400

    Add type specification for argument

--- src/datastructure/daytimerange.py
+++ src/datastructure/daytimerange.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 class TimeRangeInDay:
     """This class denotes the range within which the algorithm performs
     its analysis. It has two attributes / reasons to change: 
@@ -5,7 +7,7 @@ class TimeRangeInDay:
 
     Args: both start_time and end_time are Time objects
     """
-    def __init__(self, start_time, end_time):
+    def __init__(self, start_time: time, end_time: time):
         self.start_time = start_time
         self.end_time = end_time
 
