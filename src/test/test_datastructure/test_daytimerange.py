7dd497cbdf6a01b9680ce2cff0adf9f0ad64a7f8
vic-lee <viclee@nyu.edu>
2019-04-17 18:23:59 -0400

    Test TimeRangeInDay should not have endtime < starttime

--- src/test/test_datastructure/test_daytimerange.py
+++ src/test/test_datastructure/test_daytimerange.py
@@ -2,18 +2,19 @@ import unittest
 from datetime import time, datetime
 from datastructure.daytimerange import TimeRangeInDay
 
+
 class TestTimeRangeInDay(unittest.TestCase):
 
     def setUp(self):
         self.timerange = TimeRangeInDay(
-            start_time=time(hour=9, minute=11, second=0), 
+            start_time=time(hour=9, minute=11, second=0),
             end_time=time(hour=10, minute=20, second=0)
         )
         return super().setUp()
 
     def tearDown(self):
         return super().tearDown()
-    
+
     def test_is_datetime_in_range(self):
         result = self.timerange.is_datetime_in_range(
             datetime(year=2000, month=1, day=1, hour=9, minute=44, second=0)
@@ -24,3 +25,12 @@ class TestTimeRangeInDay(unittest.TestCase):
             datetime(year=2000, month=1, day=1, hour=17, minute=23, second=0)
         )
         self.assertFalse(result)
+
+        self._test_raise_err_on_endtime_earlier_than_starttime()
+
+    def _test_raise_err_on_endtime_earlier_than_starttime(self):
+        with self.assertRaises(ValueError):
+            TimeRangeInDay(
+                start_time=time(hour=9, minute=11, second=0),
+                end_time=time(hour=7, minute=11, second=0)
+            )
