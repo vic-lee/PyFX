ad127f8a385503f5c0e16dd61c72f35bd0aa5924
vic-lee <viclee@nyu.edu>
2019-04-17 18:24:25 -0400

    Raise error if endtime < starttime

--- src/datastructure/daytimerange.py
+++ src/datastructure/daytimerange.py
@@ -1,5 +1,6 @@
 from datetime import datetime, time
 
+
 class TimeRangeInDay:
     """This class denotes the range within which the algorithm performs
     its analysis. It has two attributes / reasons to change: 
@@ -7,20 +8,20 @@ class TimeRangeInDay:
 
     Args: both start_time and end_time are Time objects
     """
+
     def __init__(self, start_time: time, end_time: time):
         self.start_time = start_time
         self.end_time = end_time
 
+        if start_time > end_time: 
+            raise ValueError("End time cannot be earlier than start time.")
 
     def is_datetime_in_range(self, datetime: datetime):
         return datetime.time() >= self.start_time and datetime.time() <= self.end_time
 
-
     def to_string_simp(self):
         return "{}{}_{}{}".format(
-            self.start_time.hour, 
-            self.start_time.minute, 
-            self.end_time.hour, 
+            self.start_time.hour,
+            self.start_time.minute,
+            self.end_time.hour,
             self.end_time.minute)
-
-        
