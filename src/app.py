e9fcc436f48842bb1cff6e9eb43f3f1df8135af5
vic-lee <viclee@nyu.edu>
2019-05-10 01:54:29 -0400

    Log messages to file and to stdout

--- src/app.py
+++ src/app.py
@@ -20,14 +20,19 @@ from metrics.minutely_data import MinutelyData
 from datastructure.daytimerange import DayTimeRange
 from datastructure.daterange import DateRange
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 handler = logging.FileHandler('app.log')
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+formatter = logging.Formatter(fmt)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+logger.addHandler(logging.StreamHandler())
 
 
 def main():
