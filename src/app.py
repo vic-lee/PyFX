cfa0c917080d26c94b7c859c4711ecb902b56133
vic-lee <viclee@nyu.edu>
2019-05-09 21:43:22 -0400

    Extract config logic to ConfigReader

--- src/app.py
+++ src/app.py
@@ -3,11 +3,12 @@ This file is the starting point of the program. It provides a bird's eye view
 of the operations carried out to generate key metrics and to export to excel. 
 """
 
-from os.path import abspath
 from datetime import datetime, time, date
-import pandas as pd
 import logging
+from os.path import abspath
+import pandas as pd
 
+from dataio.configreader import ConfigReader
 from dataio.datareader import DataReader
 from dataio.datawriter import DataWriter
 from dataio.dfbundler import DataFrameBundler
@@ -16,7 +17,7 @@ from metrics.max_price_movements import MaxPriceMovements
 from metrics.period_price_avg import PeriodPriceAvg
 from metrics.minutely_data import MinutelyData
 
-from datastructure.daytimerange import TimeRangeInDay
+from datastructure.daytimerange import DayTimeRange
 from datastructure.daterange import DateRange
 
 logger = logging.getLogger(__name__)
@@ -31,29 +32,26 @@ logger.addHandler(handler)
 
 def main():
     """
-    This function houses the application logic. At the highest level, the 
-    program performs the same analysis for each currency pair, stored in 
-    the array. 
+    This function houses the application logic. At the highest level, the
+    program performs the same analysis for each currency pair, stored in
+    the array.
     """
     start_time = datetime.now()
 
-    currency_pairs = [
-        "AUDUSD", "EURUSD", "GBPUSD", "NZDUSD", "USDCAD",
-        "USDCHF", "USDJPY", "USDMXN", "USDTRY", "USDZAR"
-    ]
+    config = ConfigReader('conf.json')
 
     fname_suffix = generate_folder_timestamp()
 
-    for currency_pair in currency_pairs:
-        analyze_currency_pair(currency_pair, fname_suffix)
+    # for currency_pair in currency_pairs:
+    #     analyze_currency_pair(currency_pair, fname_suffix)
 
-    # analyze_currency_pair("AUDUSD", fname_suffix)
+    analyze_currency_pair("GBPUSD", fname_suffix, config)
 
     end_time = datetime.now()
     logger.info("\nProgram runtime: {}".format((end_time - start_time)))
 
 
-def analyze_currency_pair(currency_pair_name, timestamp) -> None:
+def analyze_currency_pair(currency_pair_name, timestamp, config: ConfigReader) -> None:
     """
     This function encapsulates the key steps to perform to generate metrics for 
     the output. Comments below detail the steps. 
@@ -72,22 +70,34 @@ def analyze_currency_pair(currency_pair_name, timestamp) -> None:
 
     '''2.1 Max Pip Movements'''
     price_movements = setup_price_movement_obj(
-        data=price_data, cp_name=currency_pair_name)
+        data=price_data, cp_name=currency_pair_name, config=config)
 
     price_movements.find_max_price_movements()
     price_movement_analyses = price_movements.to_benchmarked_results()
     df_dict = {**df_dict, **price_movement_analyses}
 
     '''2.2 Selected Minute Data'''
-    selected_minute_data = include_minutely_data(
-        data=price_data, cp_name=currency_pair_name)
-    df_dict["Selected Minute Data"] = selected_minute_data
+    if config.should_include_minutely_data:
+
+        selected_minute_data = include_minutely_data(
+            data=price_data, cp_name=currency_pair_name, config=config)
+        df_dict["Selected Minute Data"] = selected_minute_data
 
     '''2.3 Price Average data from range'''
-    price_avg_data, timerange = include_period_avg_data(
-        price_data, currency_pair_name)
-    column_str = str(timerange.start_time) + "_" + str(timerange.end_time)
-    df_dict[column_str] = price_avg_data
+    if config.should_include_minutely_data:
+
+        for avg_data_section in config.period_average_data_sections:
+
+            price_avg_data, timerange = include_period_avg_data(
+                price_data,
+                currency_pair_name,
+                config=config,
+                avgrange=avg_data_section
+            )
+            column_str = str(timerange.start_time) + \
+                "_" + str(timerange.end_time)
+
+            df_dict[column_str] = price_avg_data
 
     '''Aggregate dataframes'''
     dfbundler = DataFrameBundler(df_dict)
@@ -99,7 +109,7 @@ def analyze_currency_pair(currency_pair_name, timestamp) -> None:
     output_writer.df_to_xlsx()
 
 
-def setup_price_movement_obj(data, cp_name) -> MaxPriceMovements:
+def setup_price_movement_obj(data, cp_name, config: ConfigReader) -> MaxPriceMovements:
     """
     This function encapsulates the definition of a `MaxPriceMovements` class, 
     which iterates to find the max pips for each day. To do this, it defines 
@@ -121,25 +131,16 @@ def setup_price_movement_obj(data, cp_name) -> MaxPriceMovements:
     """
 
     pip_movement_config = {
-        MaxPriceMovements.TIME_RANGE: TimeRangeInDay(
-            start_time=time(hour=14, minute=30),
-            end_time=time(hour=15, minute=2)
-        ),
-        MaxPriceMovements.DATE_RANGE: DateRange(
-            start_date=date(year=2018, month=1, day=1),
-            end_date=date(year=2018, month=12, day=31)
-        ),
-        MaxPriceMovements.BENCHMARK_TIMES: [
-            time(hour=14, minute=30),
-            time(hour=14, minute=45)
-        ],
-        MaxPriceMovements.CURRENCY_PAIR: cp_name
+        MaxPriceMovements.TIME_RANGE:       config.time_range,
+        MaxPriceMovements.DATE_RANGE:       config.date_range,
+        MaxPriceMovements.BENCHMARK_TIMES:  config.benchmark_times,
+        MaxPriceMovements.CURRENCY_PAIR:    cp_name
     }
 
     return MaxPriceMovements(price_dfs=data, config=pip_movement_config)
 
 
-def include_minutely_data(data, cp_name: str) -> pd.DataFrame:
+def include_minutely_data(data, cp_name: str, config: ConfigReader) -> pd.DataFrame:
     """
     This function defines what minutely data to include in the output. 
     The minutely data to include can be specified by two dimensions: 
@@ -155,66 +156,30 @@ def include_minutely_data(data, cp_name: str) -> pd.DataFrame:
 
     minute_data = MinutelyData(
         price_dfs=data,
-        time_range=TimeRangeInDay(
-            start_time=time(hour=14, minute=30),
-            end_time=time(hour=15, minute=2)
-        ),
-        date_range=DateRange(
-            start_date=date(year=2018, month=1, day=1),
-            end_date=date(year=2018, month=12, day=31)
-        ),
+        time_range=config.time_range,
+        date_range=config.date_range,
         cp_name=cp_name,
-        specs=[
-            {
-                "range_start": time(hour=14, minute=49),
-                "range_end": time(hour=15, minute=2),
-                "include": ["Close"]
-            },
-            {
-                "range_start": time(hour=15, minute=30),
-                "range_end": time(hour=15, minute=30),
-                "include": ['Close']
-            },
-            {
-                "range_start": time(hour=15, minute=45),
-                "range_end": time(hour=15, minute=45),
-                "include": ['Close']
-            },
-        ]).to_df()
+        specs=config.minutely_data_sections,
+    ).to_df()
 
     return minute_data
 
 
-def include_period_avg_data(data, cp_name: str):
+def include_period_avg_data(data, cp_name: str, config: ConfigReader, avgrange: DayTimeRange):
     """
     This function calculates and returns a series of average prices, given the
     starting time and ending time for the calculation period. 
     """
 
-    avg_time_range_start = time(hour=14, minute=58)
-    avg_time_range_end = time(hour=15, minute=2)
     df = PeriodPriceAvg(
         price_dfs=data,
         cp_name=cp_name,
-        time_range=TimeRangeInDay(
-            start_time=time(hour=14, minute=30),
-            end_time=time(hour=15, minute=2)
-        ),
-        date_range=DateRange(
-            start_date=date(year=2018, month=1, day=1),
-            end_date=date(year=2018, month=12, day=31)
-        ),
-        time_range_for_avg=TimeRangeInDay(
-            start_time=avg_time_range_start,
-            end_time=avg_time_range_end
-        ),
-        include_open={
-            time(hour=14, minute=55),
-            time(hour=14, minute=56),
-        }
+        time_range=config.time_range,
+        date_range=config.date_range,
+        time_range_for_avg=avgrange,
     ).to_df()
 
-    return df, TimeRangeInDay(start_time=avg_time_range_start, end_time=avg_time_range_end)
+    return df, DayTimeRange(start_time=avgrange.start_time, end_time=avgrange.end_time)
 
 
 def read_price_data(currency_pair_name) -> dict:
@@ -233,7 +198,8 @@ def read_price_data(currency_pair_name) -> dict:
 
     in_fpaths = {
         DataReader.FIX: abspath("../data/datasrc/fix1819.csv"),
-        DataReader.MINUTELY: abspath("../data/datasrc/{}_Minute.csv".format(currency_pair_name)),
+        # DataReader.MINUTELY: abspath("../data/datasrc/{}_Minute.csv".format(currency_pair_name)),
+        DataReader.MINUTELY: abspath("../data/datasrc/DAT_XLSX_GBPUSD_M1_2018.csv"),
         DataReader.DAILY: abspath(
             "../data/datasrc/{}_Daily.xlsx".format(currency_pair_name))
     }
