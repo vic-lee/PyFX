ff227d93697f2e21e078bea13398bf09ce16b66c
vic-lee <viclee@nyu.edu>
2019-05-12 11:11:17 -0400

    Accomodate data dependency injection

--- src/metrics/metric.py
+++ src/metrics/metric.py
@@ -6,9 +6,11 @@ from datetime import datetime, timedelta, time
 
 from dataio.configreader import ConfigReader
 from dataio.datareader import DataReader
-from datastructure.pricetime import PriceTime
-from datastructure.daytimerange import DayTimeRange
+
+from datastructure.datacontainer import DataContainer
 from datastructure.daterange import DateRange
+from datastructure.daytimerange import DayTimeRange
+from datastructure.pricetime import PriceTime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -16,13 +18,13 @@ logger.setLevel(logging.INFO)
 
 class Metric:
 
-    def __init__(self, price_dfs, currency_pair_name: str, config: ConfigReader):
+    def __init__(self, currency_pair_name: str, config: ConfigReader):
 
         self.time_range = config.time_range
         self.date_range = config.date_range
         self.currency_pair_name = currency_pair_name
 
-    def _get_prior_fix_recursive(self, d):
+    def _get_prior_fix_recursive(self, d, prices: DataContainer):
 
         def daydelta(d, delta): return d - timedelta(days=delta)
 
@@ -33,7 +35,7 @@ class Metric:
             try:
                 cp_identifier = self.currency_pair_name[:3] + \
                     '-' + self.currency_pair_name[3:]
-                fx = self.fix_price_df.loc[str(d)][cp_identifier]
+                fx = prices.fix_price_df.loc[str(d)][cp_identifier]
             except Exception as e:
                 logger.error(
                     "Could not locate the previous fix price, possibly due to out of bounds." + str(e))
@@ -43,7 +45,7 @@ class Metric:
                                  day=d.day, hour=0, minute=0, second=0)
                 return PriceTime(price=fx, datetime=index)
             else:
-                return self._get_prior_fix_recursive(daydelta(d, 1))
+                return self._get_prior_fix_recursive(daydelta(d, 1), prices)
 
         else:
             return None
@@ -51,11 +53,15 @@ class Metric:
     @staticmethod
     def incr_one_min(time_cur):
         old_min = time_cur.minute
+
         if old_min == 59:
             new_min = 0
             new_hour = time_cur.hour + 1
+
         else:
             new_min = old_min + 1
             new_hour = time_cur.hour
+
         time_cur = time(hour=new_hour, minute=new_min, second=time_cur.second)
+
         return time_cur
