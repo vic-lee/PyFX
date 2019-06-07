f5250e9100e6e688b96c52716b94b022bf1d6be1
vic-lee <viclee@nyu.edu>
2019-06-06 22:46:12 -0400

    Make data members private

--- src/datastructure/pricetime.py
+++ src/datastructure/pricetime.py
@@ -1,26 +1,27 @@
 from datetime import datetime
 
+
 class PriceTime:
     def __init__(self, price: float, datetime: datetime):
-        self._price = price
-        self._datetime = datetime
+        self.__price = price
+        self.__datetime = datetime
 
     @property
     def datetime(self):
-        return self._datetime
+        return self.__datetime
 
     @property
     def price(self):
-        return self._price
+        return self.__price
 
     def _delta_from(self, other_price):
-        return (self._price - other_price.price)
+        return (self.__price - other_price.price)
 
     def pip_movement_from(self, other_price):
         return round(self._delta_from(other_price) * 10000, 1)
 
     def is_earlier_than(self, other_price) -> bool:
-        return self._datetime < other_price.datetime
+        return self.__datetime < other_price.datetime
 
     def is_later_than(self, other_price) -> bool:
-        return self._datetime > other_price.datetime
+        return self.__datetime > other_price.datetime
