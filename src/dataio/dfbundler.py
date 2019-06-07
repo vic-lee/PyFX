2d3745b9f58c925c28e8a640a9be9209ff2db872
vic-lee <viclee@nyu.edu>
2019-06-06 22:47:33 -0400

    Make data members of dfbundler private

--- src/dataio/dfbundler.py
+++ src/dataio/dfbundler.py
@@ -4,15 +4,15 @@ import numpy as np
 class DataFrameBundler:
     
     def __init__(self, df_dict):
-        self._df_dict = df_dict
-        self._merged_df = self._merge_dfs()
+        self.__df_dict = df_dict
+        self.__merged_df = self._merge_dfs()
 
     
     def _merge_dfs(self):
         target = pd.DataFrame()
         target.columns = pd.MultiIndex.from_product([[""], target.columns])
 
-        for header_name, df in self._df_dict.items():
+        for header_name, df in self.__df_dict.items():
             df.columns = pd.MultiIndex.from_product([[header_name], df.columns])
             target = target.join(df, how="right")
 
@@ -21,4 +21,4 @@ class DataFrameBundler:
 
 
     def output(self) -> pd.DataFrame:
-        return self._merged_df
+        return self.__merged_df
