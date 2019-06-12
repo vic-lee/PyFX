7caa83979f93cc489f63081fe808a1f3efae9cc0
vic-lee <viclee@nyu.edu>
2019-06-12 20:10:48 +0100

    Bug fix: pass param to wrapper

--- src/common/decorators.py
+++ src/common/decorators.py
@@ -7,7 +7,7 @@ def singleton(cls, *args, **kwargs):
 
     instances = {}
 
-    def wrapper():
+    def wrapper(*args, **kwargs):
         if cls not in instances:
             instances[cls] = cls(*args, **kwargs)
         return instances[cls]
