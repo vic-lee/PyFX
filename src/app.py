fca7cbe38747765f28c5eb149b26825edfe6931a
vic-lee <viclee@nyu.edu>
2019-06-17 13:19:52 -0400

    Factor out io logic

--- src/app.py
+++ src/app.py
@@ -35,31 +35,44 @@ logger.addHandler(stream_handler)
 DEFAULT_CONFIG_FPATH = 'config.json'
 
 
+def io(func):
+    def wrapper(*args, **kwargs):
+        cp_name = kwargs.get('cp_name')
+        config = kwargs.get('config')
 
-def exec(cp_name: str, config: Config):
-    fpaths = {
-        read.MINUTE:    abspath("data/datasrc/GBPUSD_Candlestick.csv"),
-        read.FIX:       abspath("data/datasrc/fix1819.csv"),
-        read.DAILY:     abspath("data/datasrc/{}_Daily.xlsx".format(cp_name))
-    }
+        fpaths = {
+            read.MINUTE:
+                abspath("data/datasrc/GBPUSD_Candlestick.csv"),
+            read.FIX:
+                abspath("data/datasrc/fix1819.csv"),
+            read.DAILY:
+                abspath("data/datasrc/{}_Daily.xlsx".format(cp_name))
+        }
 
-    dfs = read.read_and_process_data(fpaths, cp_name=cp_name)
-    data = DataContainer(dfs, cp_name, config)
+        dfs = read.read_and_process_data(fpaths, cp_name=cp_name)
+        data = DataContainer(dfs, cp_name, config)
+
+        df_master = func(*args, **kwargs, data=data)
+
+        write.df_to_xlsx(df=df_master,
+                         dir='data/dataout/', folder_name='dataout_',
+                         fname=('dataout_{}'.format(cp_name)),
+                         folder_unique_id=datetime.now().strftime("_%Y%m%d_%H%M%S"),
+                         sheet_name='max_pip_mvmts', col_width=20)
+    return wrapper
+
+
+@io
+def exec(cp_name: str, config: Config, **kwargs):
+
+    data = kwargs.get('data')
 
     output_funcs = [
-        run(analysis.include_ohlc, data=data),
-
-        run(analysis.include_max_pip_movements,
-            data, config.benchmark_times),
-
-        run(analysis.include_max_pip_movements,
-            data, pdfx=True, cp_name=cp_name),
-
-        run(analysis.include_minute_data, data,
-            config.minutely_data_sections),
-
-        run(analysis.include_period_avgs, data,
-            config.period_average_data_sections)
+        run(analysis.include_ohlc, data),
+        run(analysis.include_max_pips, data, config.benchmark_times),
+        run(analysis.include_max_pips, data, pdfx=True, cp_name=cp_name),
+        run(analysis.include_minute_data, data, config.minutely_data_sections),
+        run(analysis.include_avgs, data, config.period_average_data_sections)
     ]
 
     outputs = map(lambda f: f(), output_funcs)
@@ -67,18 +80,15 @@ def exec(cp_name: str, config: Config):
     df_master = pd.concat(outputs, axis=1)
     df_master.index = df_master.index.date
 
-    write.df_to_xlsx(df=df_master,
-                     dir='data/dataout/', folder_name='dataout_',
-                     fname=('dataout_{}'.format(cp_name)),
-                     folder_unique_id=datetime.now().strftime("_%Y%m%d_%H%M%S"),
-                     sheet_name='max_pip_mvmts', col_width=20)
+    return df_master
 
 
 @timer
 def main():
     config = Config(DEFAULT_CONFIG_FPATH)
+
     for cp in config.currency_pairs:
-        exec(cp, config)
+        exec(cp_name=cp, config=config)
 
 
 if __name__ == '__main__':
