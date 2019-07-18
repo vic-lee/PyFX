a00688b83f4e6679f775f84ac1ae4a6c75d83d0e
Vic Lee <viclee@nyu.edu>
2019-07-18 00:09:25 -0400

    Implement config benchmark time tests

--- tests/test_common/test_config.py
+++ tests/test_common/test_config.py
@@ -1,5 +1,7 @@
 import pytest
 
+import yaml
+
 from pathlib import Path
 
 from tests.context import common
@@ -26,8 +28,17 @@ def test_config_handles_file_type_error():
 
     for tf in testfiles:
         tf.touch()
-
         with pytest.raises(config.ConfigFileTypeError):
             cfg = Config(tf)
-
         tf.unlink()
+
+
+def test_config_property_benchmark_time():
+    """Tests Config loads benchmark time correctly"""
+    cfgpath = Path.cwd() / 'tests' / 'testdata' / 'config' / 'cfg1.yml'
+    with open(cfgpath) as cfg:
+        expected_cfg = yaml.safe_load(cfg)
+        test_cfg = Config(cfgpath)
+        for bt in test_cfg.benchmark_times:
+            assert bt.strftime('%H:%M') in \
+                expected_cfg['setup']['benchmark_times']
